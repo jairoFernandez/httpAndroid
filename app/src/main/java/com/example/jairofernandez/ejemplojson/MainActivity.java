32321380156fc1876290df2412068718ce63ebc1
tucompualdia <jairog12@outlook.com>
2015-07-23 17:05:58 -0500

    Clases listas

--- app/src/main/java/com/example/jairofernandez/ejemplojson/MainActivity.java
+++ app/src/main/java/com/example/jairofernandez/ejemplojson/MainActivity.java
@@ -5,21 +5,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import org.apache.http.Header;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import com.loopj.android.http.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class MainActivity extends ActionBarActivity {
 
+    ListView listado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listado = (ListView)findViewById(R.id.lvResultados);
         final EditText etNombre = (EditText)findViewById(R.id.etNombre);
         final EditText etEdad = (EditText)findViewById(R.id.etEdad);
         final EditText etHobbies = (EditText)findViewById(R.id.etHobbies);
@@ -67,8 +77,51 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
+
+
     public void ObtDatos(){
         AsyncHttpClient client = new AsyncHttpClient();
+        String url ="http://multikloset.tucompualdia.net/kloset/catalogo";
+        RequestParams parametros = new RequestParams();
+        parametros.put("Android","Android");
+        client.post(url, parametros, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200) {
+                    CargaLista(obtDatosJSON(new String(responseBody)));
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+
+    public void CargaLista(ArrayList<String> datos){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datos);
+        listado.setAdapter(adapter);
+    }
+
+    public ArrayList<String> obtDatosJSON(String response){
+        ArrayList<String> listado = new ArrayList<String>();
+        try{
+            JSONArray resulArray = new JSONArray(response);
+            String texto;
+
+            for (int i=0; i<resulArray.length();i++)
+            {
+                texto = resulArray.getJSONObject(i).getString("tipoLamina")+" "+
+                        resulArray.getJSONObject(i).getString("alto")+" "+
+                        resulArray.getJSONObject(i).getString("ancho");
+                listado.add(texto);
+            }
+
+        }catch (Exception e){
+
+        }
+        return listado;
     }
 
 
