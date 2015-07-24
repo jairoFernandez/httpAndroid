5b7b061d81804d60ed1a4e175448cad52520dabc
tucompualdia <jairog12@outlook.com>
2015-07-24 17:12:08 -0500

    ajustes

--- app/src/main/java/com/example/jairofernandez/ejemplojson/MainActivity.java
+++ app/src/main/java/com/example/jairofernandez/ejemplojson/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jairofernandez.ejemplojson;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +17,7 @@ import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import com.loopj.android.http.*;
 
 import java.lang.reflect.Array;
@@ -28,16 +30,24 @@ public class MainActivity extends ActionBarActivity {
     ListView listado;
     ArrayAdapter<Lamina> adapter;
     List<Lamina> registros;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
+        listado = (ListView) findViewById(R.id.lvResultados);
+        long numberRegistro = Lamina.count(Lamina.class, null, null);
 
+        if (numberRegistro == 0) {
+            ObtDatos();
+            registros = Lamina.findWithQuery(Lamina.class, "SELECT TIPO_LAMINA FROM Lamina");
+        }else{
+            CargarLista();
+        }
 
-        CargarLista();
-        Button btnWebService = (Button)findViewById(R.id.btnWebService);
+        Button btnWebService = (Button) findViewById(R.id.btnWebService);
 
         btnWebService.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +55,6 @@ public class MainActivity extends ActionBarActivity {
                 ObtDatos();
             }
         });
-
 
 
     }
@@ -58,18 +67,18 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-
-    public void ObtDatos(){
+    public void ObtDatos() {
         AsyncHttpClient client = new AsyncHttpClient();
-        String url ="http://multikloset.tucompualdia.net/kloset/catalogo";
+        String url = "http://multikloset.tucompualdia.net/kloset/catalogo";
         RequestParams parametros = new RequestParams();
-        parametros.put("Android","Android");
+        parametros.put("Android", "Android");
         client.post(url, parametros, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 if (statusCode == 200) {
-                   // CargaLista(obtDatosJSON(new String(responseBody)));
+                    // CargaLista(obtDatosJSON(new String(responseBody)));
                     obtDatosJSON(new String(responseBody));
+
                 }
             }
 
@@ -80,21 +89,26 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-    public void CargarLista(){
-        registros = Lamina.listAll(Lamina.class);
-        listado = (ListView)findViewById(R.id.lvResultados);
-        if(registros == null){
+    public void CargarLista() {
+        //registros = Lamina.findAll(Lamina.class);
+        //registros = Lamina.listAll(Lamina.class);
+
+        registros = Lamina.findWithQuery(Lamina.class, "SELECT TIPO_LAMINA FROM Lamina");
+        // List<Note> notes = Note.findWithQuery(Note.class, "Select * from Note where name = ?", "satya");
+
+        if (registros == null) {
             ObtDatos();
-            registros = Lamina.listAll(Lamina.class);
+            registros = Lamina.findWithQuery(Lamina.class, "SELECT TIPO_LAMINA FROM Lamina");
+            //registros = Lamina.listAll(Lamina.class);
         }
-        adapter = new ArrayAdapter<Lamina>(this.getApplicationContext(),R.layout.abc_list_menu_item_layout,R.id.lvResultados,registros);
+        adapter = new ArrayAdapter<Lamina>(this.getApplicationContext(), R.layout.abc_list_menu_item_layout, R.id.lvResultados, registros);
         //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datos);
         listado.setAdapter(adapter);
     }
 
-    public ArrayList<String> obtDatosJSON(String response){
+    public ArrayList<String> obtDatosJSON(String response) {
         ArrayList<String> listado = new ArrayList<String>();
-        try{
+        try {
             JSONArray resulArray = new JSONArray(response);
             String texto;
             String tipoLamina;
@@ -102,27 +116,31 @@ public class MainActivity extends ActionBarActivity {
             Integer alto;
             Integer valor;
             Integer valorCm;
-            for (int i=0; i<resulArray.length();i++)
-            {
-                texto = resulArray.getJSONObject(i).getString("tipoLamina")+" ("+
-                        resulArray.getJSONObject(i).getString("alto")+"X"+
-                        resulArray.getJSONObject(i).getString("ancho")+")";
+            Lamina.deleteAll(Lamina.class);
+            for (int i = 0; i < resulArray.length(); i++) {
+                texto = resulArray.getJSONObject(i).getString("tipoLamina") + " (" +
+                        resulArray.getJSONObject(i).getString("alto") + "X" +
+                        resulArray.getJSONObject(i).getString("ancho") + ")";
                 listado.add(texto);
 
                 tipoLamina = resulArray.getJSONObject(i).getString("tipoLamina");
-                ancho = Integer.getInteger(resulArray.getJSONObject(i).getString("ancho"));
-                alto = Integer.getInteger(resulArray.getJSONObject(i).getString("alto"));
-                valor = Integer.getInteger(resulArray.getJSONObject(i).getString("valor"));
-                valorCm = Integer.getInteger(resulArray.getJSONObject(i).getString("valorCm"));
+                ancho = Integer.parseInt(resulArray.getJSONObject(i).getString("ancho"));
+                alto = Integer.parseInt(resulArray.getJSONObject(i).getString("alto"));
+                valor = Integer.parseInt(resulArray.getJSONObject(i).getString("valor"));
+                valorCm = Integer.parseInt(resulArray.getJSONObject(i).getString("valorCm"));
 
-                Lamina lamina = new Lamina(tipoLamina,ancho,alto,valor,valorCm);
-
+                Lamina lamina = new Lamina(tipoLamina, ancho, alto, valor, valorCm);
                 lamina.save();
+                Log.d("results", texto);
             }
 
-        }catch (Exception e){
+            CargarLista();
+
+        } catch (Exception e) {
 
         }
+
+
         return listado;
     }
 
